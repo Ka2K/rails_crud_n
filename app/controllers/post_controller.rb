55c6aacaeabb7c47f82b18805d0bd0bf4e4428ac
Ka2K <cjswokdy@gmail.com>
2018-06-19 05:20:14 +0000

    CRUD_C

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,5 +7,12 @@ class PostController < ApplicationController
   end
 
   def create
+    #1. title: ~~ == :title => ~~
+    Post.create(title: params[:title], body: params[:body])
+    #2. 객체조작을 통해 create
+    # post = Post.new #객체에서 하나의 비어있는 인스턴스를 만들고  그 안에 값을 넣음
+    # post.title = params[:title]
+    # post.body = params[:body]
+    # post.save
   end
 end
